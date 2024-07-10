dc0110ac07925faeb5d02ba1c46952ca645511fa
hyuna <kook980316@gmail.com>
2024-07-10 16:23:31 +0900

    feat: Restaurant model 추가

--- Main/src/Restaurant/RestaurantModel/RestaurantModel.java
+++ Main/src/Restaurant/RestaurantModel/RestaurantModel.java
@@ -1,4 +1,9 @@
 package Restaurant.RestaurantModel;
 
 public class RestaurantModel {
+    private int id;
+    private String restaurantName;
+    private String category;
+    private String location;
+    private String phoneNum;
 }
