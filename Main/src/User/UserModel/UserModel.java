6484c7d06ce75bef672715d7873b9b13ccfe675f
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-10 16:18:04 +0900

    feat : UserModel 생성

--- Main/src/User/UserModel/UserModel.java
+++ Main/src/User/UserModel/UserModel.java
@@ -1,4 +1,59 @@
 package User.UserModel;
 
 public class UserModel {
+
+    // 실습때 USER
+    private int id;
+    private String nickname;
+    private String username;
+    private String password;
+    private String address;
+
+    public UserModel(int id, String nickname, String username, String password, String address) {
+        this.id = id;
+        this.nickname = nickname;
+        this.username = username;
+        this.password = password;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
