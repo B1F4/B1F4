8a523eea127e421de81a72dd2bfabed28a3c722f
hyuna <kook980316@gmail.com>
2024-07-11 16:41:15 +0900

    feat: RestaurantController 생성

--- Main/src/Restaurant/RestaurantController/RestaurantController.java
+++ Main/src/Restaurant/RestaurantController/RestaurantController.java
@@ -1,4 +1,22 @@
 package Restaurant.RestaurantController;
 
+import Restaurant.RestaurantModel.RestaurantModel;
+import Restaurant.RestaurantRepository.RestaurantRepository;
+import Restaurant.RestaurantService.RestaurantService;
+
+import java.util.List;
+
 public class RestaurantController {
+
+    public RestaurantController(String category) {
+        RestaurantRepository repository = new RestaurantRepository();
+        RestaurantService service = new RestaurantService(repository);
+
+        List<RestaurantModel> filteredRestaurants = service.getRestaurants(category);
+
+        service.printRestaurantList(filteredRestaurants);
+
+    }
+
+
 }
