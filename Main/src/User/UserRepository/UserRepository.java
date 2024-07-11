da6f5b3f3a47a6e7c160f54d9d99b259549aeb68
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-11 16:36:37 +0900

    feat: userRepository 추가

--- Main/src/User/UserRepository/UserRepository.java
+++ Main/src/User/UserRepository/UserRepository.java
@@ -1,5 +1,13 @@
 package User.UserRepository;
+import User.UserModel.UserModel;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserRepository {
     // 해시맵 DAO
+    private Map<Integer, UserModel> userMap = new HashMap<>();
+
+    public void save(int id, UserModel userModel){
+        userMap.put(id, userModel);
+    }
 }
