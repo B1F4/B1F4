61ecf29f4d096944f43bb5a909663e6579871d51
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-11 16:36:20 +0900

    feat: userService 추가

--- Main/src/User/UserService/UserService.java
+++ Main/src/User/UserService/UserService.java
@@ -1,5 +1,15 @@
 package User.UserService;
 
+import User.UserModel.UserModel;
+import User.UserRepository.UserRepository;
+
 public class UserService {
     // 로직 처리
+    int id = 1;
+    UserRepository userRepository = new UserRepository();
+
+    public void register(UserModel userModel){
+        userRepository.save(id, userModel);
+        id++;
+    }
 }
