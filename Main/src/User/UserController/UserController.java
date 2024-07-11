36f02b45e9ae7bd3d38e5aafb07fee8ce581e3fe
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-11 16:30:45 +0900

    feat : controller signup 추가

--- Main/src/User/UserController/UserController.java
+++ Main/src/User/UserController/UserController.java
@@ -1,5 +1,14 @@
 package User.UserController;
 
+import User.UserModel.UserModel;
+import User.UserService.UserService;
+
 public class UserController {
+    UserService userService = new UserService();
+
+    public void signup(String username, String password, String nickname, String address){
+        UserModel userModel = new UserModel(username, password, nickname, address);
+        userService.register(userModel);
+    }
     // 이건 컨트롤러..
 }
