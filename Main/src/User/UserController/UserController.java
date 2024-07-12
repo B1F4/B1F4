add5b7bd76f3c2e4279e9a7e1000a02041ae3c8a
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-12 16:15:06 +0900

    feat : login, update, delete 기능 추가

--- Main/src/User/UserController/UserController.java
+++ Main/src/User/UserController/UserController.java
@@ -5,10 +5,24 @@ import User.UserService.UserService;
 
 public class UserController {
     UserService userService = new UserService();
+    int id = 1;
 
     public void signup(String username, String password, String nickname, String address){
-        UserModel userModel = new UserModel(username, password, nickname, address);
-        userService.register(userModel);
+        UserModel userModel = new UserModel(id, username, password, nickname, address);
+        userService.register(id, userModel);
+        id++;
+    }
+
+    public UserModel login(String username, String password) {
+        return userService.login(username, password);
+    }
+
+    public boolean update(int id, String username, String password, String nickname, String address) {
+        return userService.update(id, username, password, nickname, address);
+    }
+
+    public void delete(int id) {
+        userService.delete(id);
     }
     // 이건 컨트롤러..
 }
