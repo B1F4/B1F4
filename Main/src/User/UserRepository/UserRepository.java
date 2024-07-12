048508aa7727aa32db733a6bcfb5b545311681cf
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-12 16:18:36 +0900

    feat : User 이름 찾기, Id 찾기 기능 추가

--- Main/src/User/UserRepository/UserRepository.java
+++ Main/src/User/UserRepository/UserRepository.java
@@ -10,4 +10,27 @@ public class UserRepository {
     public void save(int id, UserModel userModel){
         userMap.put(id, userModel);
     }
+
+    public UserModel findUsername(String username) {
+        for (UserModel user : userMap.values()) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public UserModel findId(int id) {
+        for (int key: userMap.keySet()){
+            UserModel userModel = userMap.get(key);
+            if ( key==id ){
+                return userModel;
+            }
+        }
+        return null;
+    }
+
+    public void delete(int id) {
+        userMap.remove(id);
+    }
 }
