8b31caae26a9497295f61584ca49b841529c25a8
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-07-12 16:19:13 +0900

    feat: Service에서 login, update, delete 추가

--- Main/src/User/UserService/UserService.java
+++ Main/src/User/UserService/UserService.java
@@ -5,11 +5,37 @@ import User.UserRepository.UserRepository;
 
 public class UserService {
     // 로직 처리
-    int id = 1;
     UserRepository userRepository = new UserRepository();
 
-    public void register(UserModel userModel){
+    public void register(int id, UserModel userModel){
         userRepository.save(id, userModel);
-        id++;
     }
+
+
+    public UserModel login(String username, String password) {
+        UserModel user = userRepository.findUsername(username);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        return null;
+    }
+
+    public boolean update(int id, String username, String password, String nickname, String address) {
+        UserModel user = userRepository.findId(id);
+        if (user != null) {
+            user.setUsername(username);;
+            user.setPassword(password);
+            user.setNickname(nickname);
+            user.setAddress(address);
+            userRepository.save(id, user);
+            return true;
+        }
+        return false;
+    }
+
+
+    public void delete(int id) {
+        userRepository.delete(id);
+    }
+
 }
