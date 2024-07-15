0d03bb188a4fa81f9dae0865c6db9ea9d0c6ef9b
hyuna <kook980316@gmail.com>
2024-07-15 09:47:20 +0900

    update: 가게 이름 변경

--- Main/src/Restaurant/RestaurantRepository/RestaurantRepository.java
+++ Main/src/Restaurant/RestaurantRepository/RestaurantRepository.java
@@ -11,8 +11,8 @@ public class RestaurantRepository {
     private static List<RestaurantModel> restaurantRepo = new ArrayList<>(
             Arrays.asList(
                     new RestaurantModel(1, "홍콩반점", "중식", "서울", "123"),
-                    new RestaurantModel(2, "양양식", "양식", "서울", "432"),
-                    new RestaurantModel(3, "떡볶이", "분식", "서울", "3215"),
+                    new RestaurantModel(2, "성수다락", "양식", "서울", "432"),
+                    new RestaurantModel(3, "동대문엽기떡볶이", "분식", "서울", "3215"),
                     new RestaurantModel(4, "자금성", "중식", "서울", "6545")
             )
     );
