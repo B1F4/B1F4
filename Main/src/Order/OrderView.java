43bad9f4d919ba454eda84caf9439a6165f725b1
liarcrown <liarcrown@naver.com>
2024-07-15 10:57:30 +0900

    feat: OrderView에 string으로 가게 및 가게 메뉴 넘기기

--- Main/src/Order/OrderView.java
+++ Main/src/Order/OrderView.java
@@ -11,7 +11,12 @@ import java.util.Scanner;
 
 public class OrderView  {
 
+    private String restaurantName;
+    private String[] menus;
+
     public OrderView(String Restaurant,String[] menus) {
+        this.restaurantName = Restaurant;
+        this.menus = menus;
 
         OrderController orderController=new OrderControllerImpl();
         HashMap<Integer,String> selectMap=new HashMap<>();
@@ -21,10 +26,12 @@ public class OrderView  {
         Scanner sc=new Scanner(System.in);
         ArrayList<String[]> orderDetails=new ArrayList<>();
 
+         System.out.println("환영합니다! " + restaurantName + "의 주문 시스템입니다.");
+
         new RestaurantController();
 
         while(true) {
-
+            displayMenu();
             System.out.println("주문 완료 및 종료:0,등록:1,(전체)삭제:2");
             int orderSelect=Integer.parseInt(sc.nextLine());
             String controlOrder=selectMap.get(orderSelect);
@@ -45,5 +52,12 @@ public class OrderView  {
         System.out.println(orderResponse);
 
         new ReviewView();
+    }
+    private void displayMenu() {
+        System.out.println("\n===== " + restaurantName + " 메뉴 =====");
+        for (int i = 0; i < menus.length; i++) {
+            System.out.println((i + 1) + ". " + menus[i]);
+        }
+        System.out.println("========================");
     }
 }
